18ab1d82b3a94cdcf8c8b4910c00f61e2d855cb6
Abderrahim Ajakka <ajakka.abderrahim@gmail.com>
2023-08-20 00:25:10 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <div>Hello</div>
+  );
+}
+
+export default App;
